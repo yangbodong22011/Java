5d2a8d84b268c7d064d3897212e7dac2f6240642
yangbodong <ybd@xiyoulinux.org>
2015-12-24 17:38:13 +0800

    java

---
+++ project/Huffman/HuffmanTree.java
@@ -0,0 +1,12 @@
+package IO.Huffman;
+
+/**
+ * Created by student on 12/23/15.
+ */
+public class HuffmanTree {
+    int name;
+    int weight;
+    int parent;
+    int lchild;
+    int rchild;
+}
