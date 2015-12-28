c24639c032c577a7e74f144ecf1283d3c7072008
yangbodong <ybd@xiyoulinux.org>
2015-12-28 12:00:02 +0800

    数据结构

---
+++ project/Huffman/HuffmanTree.java
@@ -0,0 +1,13 @@
+package IO.Huffman;
+
+/**
+ * Created by student on 12/23/15.
+ */
+public class HuffmanTree {
+    int name;          //节点代表的实际数字
+    int weight;        //节点的权值
+    int parent;        //父节点
+    int lchild;        //左孩子
+    int rchild;        //右孩子
+    String code;       //对应的编码
+}
